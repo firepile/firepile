a15f780699bd55b126a9abcdbf2cc258685483c8
Nate Nystrom <nate@nanocow.com>
2010-05-19 20:25:09 +0000

    A start at arrays implemented with a backing ByteBuffer.  To create a BBArray[A], one must provide a Marshal[A].  Marshals are defined for most of the primitives and for Pair and Triple.  Can maybe generate marshals at run-time.

---
+++ src/simplecl/util/BufferBackedArray.scala
@@ -0,0 +1,254 @@
+object BufferBackedArray {
+  import java.nio.ByteBuffer 
+  import scala.collection.mutable.ArrayLike
+  import scala.collection.mutable.Builder
+
+  abstract class Marshal[A: ClassManifest] {
+    def size: Int
+    def align: Int
+    // require(align <= size)
+
+    def put(buf: ByteBuffer, i: Int, x: A): Unit
+    def get(buf: ByteBuffer, i: Int): A
+  }
+
+  implicit object ZM extends Marshal[Boolean] {
+    val size = 1
+    val align = 1
+    def put(buf:ByteBuffer, i: Int, x: Boolean) = buf.put(i, if (x) 1 else 0)
+    def get(buf:ByteBuffer, i: Int) = if (buf.get(i) == 0) false else true
+  }
+
+  implicit object BM extends Marshal[Byte] {
+    val size = 1
+    val align = 1
+    def put(buf:ByteBuffer, i: Int, x: Byte) = buf.put(i, x)
+    def get(buf:ByteBuffer, i: Int) = buf.get(i)
+  }
+
+  implicit object SM extends Marshal[Short] {
+    val size = 2
+    val align = 2
+    def put(buf:ByteBuffer, i: Int, x: Short) = buf.putShort(i, x)
+    def get(buf:ByteBuffer, i: Int) = buf.getShort(i)
+  }
+
+  implicit object CM extends Marshal[Char] {
+    val size = 2
+    val align = 2
+    def put(buf:ByteBuffer, i: Int, x: Char) = buf.putChar(i, x)
+    def get(buf:ByteBuffer, i: Int) = buf.getChar(i)
+  }
+
+  implicit object IM extends Marshal[Int] {
+    val size = 4
+    val align = 4
+    def put(buf:ByteBuffer, i: Int, x: Int) = buf.putInt(i, x)
+    def get(buf:ByteBuffer, i: Int) = buf.getInt(i)
+  }
+
+  implicit object LM extends Marshal[Long] {
+    val size = 8
+    val align = 8
+    def put(buf:ByteBuffer, i: Int, x: Long) = buf.putLong(i, x)
+    def get(buf:ByteBuffer, i: Int) = buf.getLong(i)
+  }
+
+  implicit object FM extends Marshal[Float] {
+    val size = 4
+    val align = 4
+    def put(buf:ByteBuffer, i: Int, x: Float) = buf.putFloat(i, x)
+    def get(buf:ByteBuffer, i: Int) = buf.getFloat(i)
+  }
+
+  implicit object DM extends Marshal[Double] {
+    val size = 8
+    val align = 8
+    def put(buf:ByteBuffer, i: Int, x: Double) = buf.putDouble(i, x)
+    def get(buf:ByteBuffer, i: Int) = buf.getDouble(i)
+  }
+
+  implicit def tuple2Marshal[A: Marshal,B:Marshal] = new T2M[A,B]
+
+  class T2M[A: Marshal,B:Marshal] extends Marshal[Tuple2[A,B]] {
+    val ma = implicitly[Marshal[A]]
+    val mb = implicitly[Marshal[B]]
+
+    lazy val size = (ma.size max mb.align) + mb.size
+    lazy val align = ma.align max mb.align
+
+    // println("tuple._1 size = " + ma.size)
+    // println("tuple._1 align = " + ma.align)
+    // println("tuple._2 size = " + mb.size)
+    // println("tuple._2 align = " + mb.align)
+    // println("tuple size = " + size)
+    // println("tuple align = " + align)
+
+    def put(buf:ByteBuffer, i: Int, x: Tuple2[A,B]) = {
+        ma.put(buf, i, x._1)
+        mb.put(buf, i + (ma.size max mb.align), x._2)
+    }
+    def get(buf:ByteBuffer, i: Int) = {
+        val fst: A = ma.get(buf, i)
+        val snd: B = mb.get(buf, i + (ma.size max mb.align))
+        (fst, snd)
+    }
+  }
+
+  implicit def tuple3Marshal[A: Marshal,B:Marshal,C:Marshal] = new T3M[A,B,C]
+
+  class T3M[A: Marshal,B:Marshal,C:Marshal] extends Marshal[Tuple3[A,B,C]] {
+    val ma = implicitly[Marshal[A]]
+    val mb = implicitly[Marshal[B]]
+    val mc = implicitly[Marshal[C]]
+
+    lazy val size = (ma.size max mb.align) + (mb.size max mc.align) + mc.align
+    lazy val align = ma.align max mb.align max mc.align
+
+    // println("tuple._1 size = " + ma.size)
+    // println("tuple._1 align = " + ma.align)
+    // println("tuple._2 size = " + mb.size)
+    // println("tuple._2 align = " + mb.align)
+    // println("tuple size = " + size)
+    // println("tuple align = " + align)
+
+    def put(buf:ByteBuffer, i: Int, x: Tuple3[A,B,C]) = {
+        ma.put(buf, i, x._1)
+        mb.put(buf, i + (ma.size max mb.align), x._2)
+        mc.put(buf, i + (ma.size max mb.align) + (mb.size max mc.align), x._3)
+    }
+    def get(buf:ByteBuffer, i: Int) = {
+        val fst: A = ma.get(buf, i)
+        val snd: B = mb.get(buf, i + (ma.size max mb.align))
+        val thd: C = mc.get(buf, i + (ma.size max mb.align) + (mb.size max mc.align))
+        (fst, snd, thd)
+    }
+  }
+
+  implicit def bbarrayCBFrom[A: Marshal, From] = new scala.collection.generic.CanBuildFrom[From,A,BBArray[A]] {
+    def apply: Builder[A, BBArray[A]] = new BBArrayBuilder[A](16*implicitly[Marshal[A]].size)
+    def apply(from: From): Builder[A, BBArray[A]] = new BBArrayBuilder[A](16*implicitly[Marshal[A]].size)
+  }
+  implicit def bbarrayCBFromArray[A: Marshal] = new scala.collection.generic.CanBuildFrom[Array[_],A,BBArray[A]] {
+    def apply: Builder[A, BBArray[A]] = new BBArrayBuilder[A](16*implicitly[Marshal[A]].size)
+    def apply(from: Array[_]): Builder[A, BBArray[A]] = new BBArrayBuilder[A](from.length*implicitly[Marshal[A]].size)
+  }
+  implicit def bbarrayCBFromBBArray[A: Marshal] = new scala.collection.generic.CanBuildFrom[BBArray[_],A,BBArray[A]] {
+    def apply: Builder[A, BBArray[A]] = new BBArrayBuilder[A](16*implicitly[Marshal[A]].size)
+    def apply(from: BBArray[_]): Builder[A, BBArray[A]] = new BBArrayBuilder[A](from.length*implicitly[Marshal[A]].size)
+  }
+
+  class BBArrayBuilder[A: Marshal](private var b: ByteBuffer) extends Builder[A, BBArray[A]] {
+    def this(n: Int) = this(ByteBuffer.allocate(n))
+
+    override def +=(elem: A) = {
+      val pos = b.position
+      val m = implicitly[Marshal[A]]
+
+      // Grow!
+      // println("cap " + b.capacity)
+      // println("limit " + b.limit)
+      // println("pos " + b.position)
+      // println("rem " + b.remaining)
+
+      if (b.capacity < pos + m.size) {
+        // println("growing at " + pos + " to " + (b.capacity + m.size) * 2)
+        val nb = ByteBuffer.allocate(b.capacity*2)
+        b = nb.put(b.rewind.asInstanceOf[ByteBuffer])
+      }
+
+      m.put(b, pos, elem)
+      b = b.position(pos+m.size).asInstanceOf[ByteBuffer]
+      // b = b.limit(pos+m.size).asInstanceOf[ByteBuffer]
+      this
+    }
+
+    def clear: Unit = { b = b.rewind.asInstanceOf[ByteBuffer] }
+
+    def result = {
+      new BBArray[A](b.flip.asInstanceOf[ByteBuffer])
+    }
+  }
+
+  class BBArray[A: Marshal](private val buf: ByteBuffer) extends ArrayLike[A, BBArray[A]] {
+    def this(n: Int) = this(ByteBuffer.allocate(n * implicitly[Marshal[A]].size)) // ByteBuffer.wrap(Array.ofDim[Byte](n * implicitly[Marshal[A]].size)))
+
+    // println("created array with buffer of " + buf.limit + " bytes")
+
+    override def newBuilder: Builder[A, BBArray[A]] = new BBArrayBuilder[A](16*marshal.size)
+
+    lazy val marshal = implicitly[Marshal[A]]
+
+    def apply(i: Int): A = marshal.get(buf, i*marshal.size)
+    def length: Int = buf.limit / marshal.size
+    def update(i: Int, x: A): Unit = marshal.put(buf, i*marshal.size, x)
+  }
+
+  def time[T](key: String)(body: => T): T = {
+    val t0 = System.currentTimeMillis
+    try {
+      body
+    }
+    finally {
+      val t1 = System.currentTimeMillis
+      println(key + " " + (t1-t0)/1000.)
+    }
+  }
+
+  def main(args: Array[String]) {
+    val n = 100000
+    val a = Array.tabulate[(Float,Double,Boolean)](n)(i => (i.toFloat,i.toDouble,i % 2 == 0))
+    val b = new BBArray[(Float,Double,Boolean)](n)
+    for (i <- 0 until n)
+      b(i) = a(i)
+
+    for (i <- 0 until n) {
+      if (a(i) != b(i))
+        println("fail at " + i + ": " + a(i) + " != " + b(i))
+    }
+
+    var y = 0.
+    time("a.sum") {
+      for (i <- 0 until 1000000) {
+        if (a(i % n)._3) {
+          y += a(i % n)._1
+          y += a(i % n)._2
+        }
+      }
+    }
+    println(y)
+
+    var x = 0.
+    time("b.sum") {
+      for (i <- 0 until 1000000) {
+        if (b(i % n)._3) {
+          x += b(i % n)._1
+          x += b(i % n)._2
+        }
+      }
+    }
+
+    val ma = time("a.map") {
+      a.map(t => t._1 + t._2)
+    }
+    // println(ma)
+
+    val ra = time("a.reduce") {
+      ma.reduceLeft[Double](_ + _)
+    }
+    println(ra)
+
+    val mb = time("b.map") {
+      b.map(t => t._1 + t._2)
+    }
+    // println(mb)
+
+    val rb = time("b.reduce") {
+      // b.reduceLeft[(Float,Double)]((t,u) => (t._1 + u._1, t._2 + u._2))
+      mb.reduceLeft[Double](_ + _)
+    }
+    println(rb)
+
+  }
+}
+
