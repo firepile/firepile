24fd81e466f50bbeb9d2a8cd558bef010b73db1e
Nate Nystrom <nate@nanocow.com>
2010-06-13 03:49:15 +0000

    Make math methods native.  Because they are :-)

--- src/main/scala/firepile/util/Math.scala
+++ src/main/scala/firepile/util/Math.scala
@@ -3,75 +3,75 @@ package firepile.util
 object Math {
   // Math functions for floats 
 
-// pi, e, etc
+  val Pi = scala.math.Pi
 
-  def acos(x: Float) = 0.0F
-  def acosh(x: Float) = 0.0F
-  def acospi(x: Float) = 0.0F   // acos(x) / pi
-  def asin(x: Float) = 0.0F
-  def asinh(x: Float) = 0.0F
-  def asinpi(x: Float) = 0.0F
-  def atan(x: Float) = 0.0F
-  def atan2(y: Float, x: Float) = 0.0F
-  def atanh(x: Float) = 0.0F
-  def atanpi(x: Float) = 0.0F
-  def atan2pi(x: Float, y: Float) = 0.0F
-  def cbrt(x: Float) = 0.0F
-  def ceil(x: Float) = 0.0F
-  def copysign(x: Float, y: Float) = 0.0F
-  def cos(x: Float) = 0.0F
-  def cosh(x: Float) = 0.0F
-  def cospi(x: Float) = 0.0F
-  def erfc(x: Float) = 0.0F
-  def erf(x: Float) = 0.0F
-  def exp(x: Float) = 0.0F
-  def exp2(x: Float) = 0.0F
-  def exp10(x: Float) = 0.0F
-  def expm1(x: Float) = 0.0F
-  def fabs(x: Float) = 0.0F
-  def fdim(x: Float, y: Float) = 0.0F
-  def floor(x: Float) = 0.0F
-  def fma(a: Float, b: Float, c: Float) = 0.0F
-  def fmax (x: Float, y: Float) = 0.0F
-  def fmin21(x: Float, y: Float) = 0.0F
-  def fmod(x: Float, y: Float) = 0.0F
+  @native def acos(x: Float): Float
+  @native def acosh(x: Float): Float
+  @native def acospi(x: Float): Float   // acos(x) / pi
+  @native def asin(x: Float): Float
+  @native def asinh(x: Float): Float
+  @native def asinpi(x: Float): Float
+  @native def atan(x: Float): Float
+  @native def atan2(y: Float, x: Float): Float
+  @native def atanh(x: Float): Float
+  @native def atanpi(x: Float): Float
+  @native def atan2pi(x: Float, y: Float): Float
+  @native def cbrt(x: Float): Float
+  @native def ceil(x: Float): Float
+  @native def copysign(x: Float, y: Float): Float
+  @native def cos(x: Float): Float
+  @native def cosh(x: Float): Float
+  @native def cospi(x: Float): Float
+  @native def erfc(x: Float): Float
+  @native def erf(x: Float): Float
+  @native def exp(x: Float): Float
+  @native def exp2(x: Float): Float
+  @native def exp10(x: Float): Float
+  @native def expm1(x: Float): Float
+  @native def fabs(x: Float): Float
+  @native def fdim(x: Float, y: Float): Float
+  @native def floor(x: Float): Float
+  @native def fma(a: Float, b: Float, c: Float): Float
+  @native def fmax (x: Float, y: Float): Float
+  @native def fmin21(x: Float, y: Float): Float
+  @native def fmod(x: Float, y: Float): Float
     // gentype fract (gentype x, gentype *iptr)
-    def fract(x: Float) = (0, 0.0F)
+    @native def fract(x: Float): (Float,Float)
     // gentype frexp (gentype x, intn *exp)
-    def frexp(x: Float) = (0.0F, 0.0F)
-  def hypot(x: Float, y: Float) = 0.0F
-  def ilogb(x: Float): Int  = { 0 } // intn ilogb (gentype x)
-  def ldexp(x: Float, n: Int) = 0.0F
-  def lgamma(x: Float): Float  = 0.0F
-  // def lgamma_r(gentype x, intn *signp) = 0.0F
-  def log(x: Float) = 0.0F
-  def log2(x: Float) = 0.0F
-  def log10(x: Float) = 0.0F
-  def logb(x: Float) = 0.0F
-  def mad(a: Float, b: Float, c: Float) = 0.0F
+    @native def frexp(x: Float): (Float,Float)
+  @native def hypot(x: Float, y: Float): Float
+  @native def ilogb(x: Float): Int // intn ilogb (gentype x)
+  @native def ldexp(x: Float, n: Int): Float
+  @native def lgamma(x: Float): Float
+  // def lgamma_r(gentype x, intn *signp): Float
+  @native def log(x: Float): Float
+  @native def log2(x: Float): Float
+  @native def log10(x: Float): Float
+  @native def logb(x: Float): Float
+  @native def mad(a: Float, b: Float, c: Float): Float
   // def modf(gentype x, gentype *iptr) 
-  def nan(nancode: Int) = 0.0F
-  def nextafter(x: Float, y: Float) = 0.0F
-  def pow(x: Float, y: Float) = 0.0F
-  def pown(x: Float, y: Int) = 0.0F
-  def powr(x: Float, y: Float) = 0.0F
-  def remainder(x: Float, y: Float) = 0.0F
+  @native def nan(nancode: Int): Float
+  @native def nextafter(x: Float, y: Float): Float
+  @native def pow(x: Float, y: Float): Float
+  @native def pown(x: Float, y: Int): Float
+  @native def powr(x: Float, y: Float): Float
+  @native def remainder(x: Float, y: Float): Float
     // gentype remquo (gentype x, gentype y, intn *quo)
-    def remquo(x: Float, y: Float) = (0.0F, 0.0F)
-  def rint(x: Float) = 0.0F
-  def rootn(x: Float, y: Int) = 0.0F
-  def round(x: Float) = 0.0F
-  def rsqrt(x: Float) = 0.0F
-  def sin(x: Float) = 0.0F
+    @native def remquo(x: Float, y: Float)
+  @native def rint(x: Float): Float
+  @native def rootn(x: Float, y: Int): Float
+  @native def round(x: Float): Float
+  @native def rsqrt(x: Float): Float
+  @native def sin(x: Float): Float
   // gentype sincos (gentype x, gentype *cosval)
     def sincos(x: Float) = (sin(x), cos(x))
-  def sinh(x: Float) = 0.0F
-  def sinpi(x: Float) = 0.0F
-  def sqrt(x: Float) = 0.0F
-  def tan(x: Float) = 0.0F
-  def tanh(x: Float) = 0.0F
-  def tanpi(x: Float) = 0.0F
-  def tgamma(x: Float) = 0.0F
-  def trunc(x: Float) = 0.0F
-  def abs(x: Float) = 0.0F
+  @native def sinh(x: Float): Float
+  @native def sinpi(x: Float): Float
+  @native def sqrt(x: Float): Float
+  @native def tan(x: Float): Float
+  @native def tanh(x: Float): Float
+  @native def tanpi(x: Float): Float
+  @native def tgamma(x: Float): Float
+  @native def trunc(x: Float): Float
+  @native def abs(x: Float): Float
 }
