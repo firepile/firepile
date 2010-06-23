9fbb5ec6357c0432bcec40baf90ecf2b71587cd8
Nate Nystrom <nate@nanocow.com>
2010-06-23 21:51:24 +0000

    Don't optimize.

--- project/build/FirepileProject.scala
+++ project/build/FirepileProject.scala
@@ -1,7 +1,8 @@
 import sbt._
 
 class FirepileProject(info: ProjectInfo) extends DefaultProject(info) with AutoCompilerPlugins {
-  override def compileOptions = super.compileOptions ++ compileOptions("-Xprint:typer -Xprint:cleanup -Xprint:specialize -Yoptimise -Ysqueeze:on -Ydead-code -Yinline")
+  //override def compileOptions = super.compileOptions ++ compileOptions("-Xlog-implicits", "-Xprint:namer", "-Xprint-types", "-Ytyper-debug", "-Ysqueeze:on", "-Yclosure-elim", "-Ydead-code", "-Yinline")
+  // override def compileOptions = super.compileOptions ++ compileOptions("-Ysqueeze:on", "-Yclosure-elim", "-Ydead-code", "-Yinline")
   // override def compileOptions = super.compileOptions ++ compileOptions("-Xlog-implicits")
 }
 
