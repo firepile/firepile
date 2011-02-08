5cb56a2070c8c937df977b8d48d0c9083a38f09b
kishendas <kishen.das@gmail.com>
2011-02-08 15:06:28 -0600

    Kernel.scala added

---
+++ src/main/scala/firepile/Kernel.scala
@@ -0,0 +1,27 @@
+package firepile
+
+//import java.util.ArrayList
+import scala.collection.mutable.HashMap
+import soot.Type
+
+
+object Kernel {
+
+def apply(t: Int, b: Int) = {
+
+threads= t
+blocks = b
+
+}
+var threads: Int =0
+var blocks: Int =0
+var level: Int = 1
+val src: String = ""
+val globalArgs = new HashMap[String,String]()
+val localArgs = new HashMap[String,(String,Int)]()
+
+}
+
+
+
+
